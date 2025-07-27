69a2d337054940b4d60df16fc22d6be8394ac41f
rdghostmann <randalchukzwilson@gmail.com>
2025-07-27 19:39:20 +0100

    updated stuff II

--- components/CategoryGrid.tsx
+++ components/CategoryGrid.tsx
@@ -15,7 +15,7 @@ export default function CategoryGrid() {
           View All Categories
         </Link>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
         {categories.map((category) => (
           <Link
             key={category.id}
