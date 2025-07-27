48e69020a37ba3bc47860ea4a8cbc932bd005936
rdghostmann <randalchukzwilson@gmail.com>
2025-07-27 20:22:18 +0100

    updated stuff

--- app/categories/[id]/subcategories/[subId]/page.tsx
+++ app/categories/[id]/subcategories/[subId]/page.tsx
@@ -1,11 +1,14 @@
 import TopNavigation from "@/components/TopNavigation";
 import MobileTabNavigation from "@/components/MobileTabNavigation";
 import SubcategoryProductList from "./SubcategoryProductList";
-
-export default function SubcategoryPage() {
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/auth"
+export default async function SubcategoryPage() {
+   const session = await getServerSession(authOptions)
+    const username = session?.user?.username || "Guest"
   return (
     <div>
-      <TopNavigation />
+      <TopNavigation username={username} />
       <SubcategoryProductList />
       <MobileTabNavigation />
     </div>
