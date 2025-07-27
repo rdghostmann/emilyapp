c1a10b52a54bfc4d237e0c27ed8ea99b63c7b67b
rdghostmann <randalchukzwilson@gmail.com>
2025-07-27 20:36:58 +0100

    updated stuff II

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,11 +1,15 @@
 import CartInterface from "@/components/CartInterface";
 import MobileTabNavigation from "@/components/MobileTabNavigation";
 import TopNavigation from "@/components/TopNavigation";
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/auth"
+export default async function CartPage() {
+  const session = await getServerSession(authOptions)
+  const username = session?.user?.username || "Guest"
 
-export default function CartPage() {
   return (
     <>
-      <TopNavigation />
+      <TopNavigation username={username} />
       <div className="min-h-screen bg-gray-50">
         <CartInterface />
       </div>
